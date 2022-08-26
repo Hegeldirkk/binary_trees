9db02c75b43bd2ca103e21b7ee5155e554fad0ba
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-26 08:51:03 +0000

    script git writting - create node binary

--- binary_trees.h
+++ binary_trees.h
@@ -21,7 +21,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+/*typedef struct binary_tree_s binary_tree_t;*/
 
 /*Prototypes code*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
