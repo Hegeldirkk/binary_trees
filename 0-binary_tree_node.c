5d7a35014bef1e24d21cd8385d55d6945d44554b
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-26 01:59:12 +0000

    script git writting - 0. New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ * Return: value, 0, fail.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	struct binary_tree_s *created_node = malloc(sizeof(struct binary_tree_s));
+
+	if (created_node == NULL)
+		return (NULL);
+
+	created_node->n = value;
+	created_node->left = NULL;
+	created_node->right = NULL;
+	created_node->parent = parent;
+	return (created_node);
+}
