cb40a5abfc24d2b7616a912acc127adb7c427d62
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-27 02:57:56 +0000

    script git writting - Post-order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - binary tree using postorder traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		if (tree->left)
+			binary_tree_postorder(tree->left, func);
+
+		if (tree->right)
+			binary_tree_postorder(tree->right, func);
+
+		func(tree->n);
+	}
+}
